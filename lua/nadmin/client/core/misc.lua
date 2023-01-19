34c06cdd9a853168cfebfb6215902be4999b7045
NubTheFatMan <that1nub@gmail.com>
2023-01-18 19:37:51 -0800

    Updated fonts to only generate once if reloaded

--- lua/nadmin/client/core/misc.lua
+++ lua/nadmin/client/core/misc.lua
@@ -8,40 +8,44 @@ end)
 
 
 -- Formerly fonts.lua
-surface.CreateFont("nadmin_derma_xs", {
-    size = 12
-})
-surface.CreateFont("nadmin_derma_small", {
-    size = 15
-})
-surface.CreateFont("nadmin_derma_small_b", {
-    size = 15,
-    weight = 800
-})
-surface.CreateFont("nadmin_derma", {
-    size = 20
-})
-surface.CreateFont("nadmin_derma_b", {
-    size = 20,
-    weight = 800,
-})
-surface.CreateFont("nadmin_derma_large", {
-    size = 32
-})
-surface.CreateFont("nadmin_derma_large_b", {
-    size = 32,
-    weight = 800
-})
-surface.CreateFont("nadmin_derma_xl", {
-    size = 40,
-    weight = 800
-})
-surface.CreateFont("nadmin_hud", {
-    size = 28
-})
-surface.CreateFont("nadmin_hud_small", {
-    size = 20
-})
+nadmin.createdFonts = nadmin.createdFonts or false 
+if not nadmin.createdFonts then 
+    nadmin.createdFonts = true
+    surface.CreateFont("nadmin_derma_xs", {
+        size = 12
+    })
+    surface.CreateFont("nadmin_derma_small", {
+        size = 15
+    })
+    surface.CreateFont("nadmin_derma_small_b", {
+        size = 15,
+        weight = 800
+    })
+    surface.CreateFont("nadmin_derma", {
+        size = 20
+    })
+    surface.CreateFont("nadmin_derma_b", {
+        size = 20,
+        weight = 800
+    })
+    surface.CreateFont("nadmin_derma_large", {
+        size = 32
+    })
+    surface.CreateFont("nadmin_derma_large_b", {
+        size = 32,
+        weight = 800
+    })
+    surface.CreateFont("nadmin_derma_xl", {
+        size = 40,
+        weight = 800
+    })
+    surface.CreateFont("nadmin_hud", {
+        size = 28
+    })
+    surface.CreateFont("nadmin_hud_small", {
+        size = 20
+    })
+end
 
 
 -- Formerly ranks.lua
